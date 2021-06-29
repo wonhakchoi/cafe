8523295d78ba89d9ced90fe176cbb8b6ef1f01ef
wonhakchoi <wonhakchoi@gmail.com>
2021-06-29 13:30:20 -0700

    installed styled-components, google fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
 
+  h1, h2, h3 {
+    font-family: 'Montserrat', sans-serif;
+  }
+`
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
-    
+    <>
+      <h1>Cafe</h1>
+      <GlobalStyle />
+      <div>
+        hello
+      </div>
+    </>
   );
 }
 
